f22905e2e228031ce40bdf7fa0105af447327ee8
peppegerace <peppegerace47@gmail.com>
2023-10-16 16:23:16 +0200

    [add] scaffolding

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import { store } from './data/store';
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getApi() {
+      axios.get(store.apiUrl)
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err =>{
+          
+        })
+    }
+  },
+  mounted() {
+    this.getApi()
+  }
+}
+</script>
+
+
+<template>
+  <h1>Boolflix</h1>
+  
+</template>
+
+
+<style lang="scss">
+
+@use './scss/main.scss' as *;
+
+</style>
